947a0918e311dbefcaf05c4dbfe125d7d4ff100d
paulo vitor <paulov14@alu.ufc.br>
2025-01-10 16:47:16 -0300

    AlarmApp

---
+++ AlarmApp/app/src/main/java/com/example/alarmapp/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.alarmapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Green80 = Color(0xFFA1C0E6)  // Verde claro
+val GreenGrey80 = Color(0xFF000DFF)  // Verde cinza claro
+val Lime80 = Color(0xFF5358FD)  // Verde limão claro
+
+val Green40 = Color(0xFFA1C0E6)  // Verde claro mais forte
+val GreenGrey40 = Color(0xFF000DFF)  // Verde cinza
+val Lime40 = Color(0xFF5358FD)  // Verde limão mais forte
---
+++ AlarmApp/app/src/main/java/com/example/alarmapp/ui/theme/Theme.kt
@@ -0,0 +1,48 @@
+package com.example.alarmapp.ui.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Green80,  // Verde claro para o tema escuro
+    secondary = GreenGrey80,  // Verde cinza claro
+    tertiary = Lime80  // Verde limão claro
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = Green40,  // Verde claro para o tema claro
+    secondary = GreenGrey40,  // Verde cinza
+    tertiary = Lime40  // Verde limão
+)
+
+@Composable
+fun AlarmAppTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
